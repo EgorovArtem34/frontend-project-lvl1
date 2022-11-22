9ec82f7b4d9d0b0f61a7f2ad961b76ae2dfeb2ed
Artem Egorov <artegtor@gmail.com>
2022-11-22 12:37:58 +0400

    fix eslint src/cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,9 @@
 import readlineSync from 'readline-sync';
-export const sayHello = () => {
-    let userName = readlineSync.question('May I have your name? ');
-    return console.log(`Hello, ${userName}!`);
+
+const sayHello = () => {
+  const userName = readlineSync.question('May I have your name? ');
+
+  return console.log(`Hello, ${userName}!`);
 };
+
+export default sayHello;
