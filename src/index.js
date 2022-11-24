4610a9045804770180c3a8603abe9e5e52d12da0
Artem Egorov <artegtor@gmail.com>
2022-11-24 09:10:51 +0400

    add src/index.js

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import readlineSync from 'readline-sync';
+
+const playGame = (rules, gameSettings) => {
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+  console.log(rules);
+  const maxCountRounds = 3;
+  for (let i = 1; i <= maxCountRounds; i += 1) {
+    const [correctAnswer, numbersForQuest] = gameSettings();
+    console.log(`Question: ${numbersForQuest}`);
+    const answerPerson = readlineSync.question('Your answer: ');
+    if (answerPerson === correctAnswer) {
+      console.log('Correct!');
+    } else {
+      console.log(`'${answerPerson}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      return console.log(`Let's try again, ${userName}!`);
+    }
+  }
+  return console.log(`Congratulations, ${userName}!`);
+};
+
+export default playGame;
