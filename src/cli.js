8453e43eef5b41df1dd00605159724829a83e2a9
Artem Egorov <artegtor@gmail.com>
2023-02-16 17:14:33 +0400

    test

--- src/cli.js
+++ src/cli.js
@@ -3,7 +3,7 @@ import readlineSync from 'readline-sync';
 const sayHello = () => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
-
+  // test
   return console.log(`Hello, ${userName}!`);
 };
 
